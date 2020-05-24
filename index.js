406a0f64cbc141f748aa8afc43ca3e4dd5a16691
Koko1232 <65800020+Koko1232@users.noreply.github.com>
2020-05-24 19:11:47 +0100

    changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
-const { Client, Util, MessageAttachment} = require("discord.js");
+const { Client, Util, MessageAttachment, MessageEmbed} = require("discord.js");
 
-const hook = new Discord.WebhookClient('713245002431594533', 'O3WmrcJvrynu2_mdNk3t9pl7lIujnKS4qTqCRx6ZD025CZsegYK7Gmt0Mo7g9detok_m');
-hook.send('Isekriyid **KoKo** <== "**ON**"'); 
+//const hook = new Discord.WebhookClient('713245002431594533', 'O3WmrcJvrynu2_mdNk3t9pl7lIujnKS4qTqCRx6ZD025CZsegYK7Gmt0Mo7g9detok_m');
+//hook.send('Isekriyid **KoKo** <== "**ON**"'); 
 const bot = new Client({
     disableMentions: "all"
 });
@@ -41,6 +41,54 @@ function getUserFromMention(mention) {
 }
 
 bot.on("message", async message=>{
+  if(message.author.bot) return;
+  console.log(message.mentions);
+  if(message.content.toLowerCase().startsWith('.stats')) {
+    const args = message.content.split(' ');
+    console.log(args);
+    if(args.length > 2) {
+      message.channel.send(`Incorrect Usage: .stats | .stats <user_id> | .stats @mention`);
+    } else if(args.length === 2) {
+      const member = message.mentions.members.size === 1 ? 
+      message.mentions.members.first() :
+      message.guild.members.cache.get(args[1]);
+      if(member) {
+        const embed = new MessageEmbed()
+          .setAuthor(`${member.user.tag} (${member.id})`, member.user.displayAvatarURL())
+          .setThumbnail(member.user.displayAvatarURL())
+          .addField('Created On', member.user.createdAt.toLocaleString(), true)
+          .addField('Joined On', member.joinedAt, true)
+          .addField('Kickable', member.kickable, false)
+          .addField('Voice Channel', member.voice.channel ? member.voice.channel.name + `(${member.voice.channel.id})` : 'None')
+          .addField('Status', member.presence.status)
+          .setDescription(`${member.roles.cache.map(role => role.toString()).join(' ')}`);
+          message.channel.send(embed);
+      } else {
+        message.channel.send(`Doufighara hbibna agui faut le **mentionner**, je trouve pas => **${args[1]}**`);
+      }
+      
+    } else {
+      const { guild } = message;
+      const embed = new MessageEmbed()
+        .setAuthor(`${guild.name} (${guild.id})`, guild.iconURL())
+        .setThumbnail(guild.iconURL())
+        .addField('Created On', guild.createdAt.toLocaleString(), true)
+        .addField('Guild Owner', guild.owner.user.tag)
+        .addField('Total Members', guild.memberCount, true)
+        .addField('Total Real Members', guild.members.cache.filter(member => !member.user.bot).size, true)
+        .addField('Total Bots', guild.members.cache.filter(member => member.user.bot).size, true)
+        .addField('Total Channels', guild.channels.cache.size, true)
+        .addField('Total Text Channels', guild.channels.cache.filter(ch => ch.type === 'text').size, true)
+        .addField('Total Voice Channels', guild.channels.cache.filter(ch => ch.type === 'voice').size, true)
+        .setColor('#5CC5FF')
+        .setDescription(`${guild.roles.cache.map(role => role.toString()).join(' ')}`);
+      message.channel.send(embed);
+    }
+  }
+});
+
+bot.on("message", async message=>{
+
     
 if(message.author.bot || message.channel.type==="dm")return;
     let prefix="."; 
@@ -53,22 +101,23 @@ if(message.author.bot || message.channel.type==="dm")return;
     if(cmd===`.cv`){
         return message.reply("oui oui cv et toi")
     }
-    if (cmd === ".userinfo" || cmd === ".ui") {
+    if (cmd === ".userinfo") {
         
        
-        let uEmbed = new Client.MessageEmbed()
-        .setColor("#7289DA")
-      .setTitle("Server Info")
-      .setThumbnail(message.guild.iconURL())
-      .setAuthor(`${message.author.username} Info`, message.author.displayAvatarURL())
-      .addField("**Ismis:**", `${message.author.username}`, true)
-      .addField("**#:**", `${message.author.discriminator}`, true)
-      .addField("**ID:**", `${message.author.id}`, true)
-      .addField("**Status:**", `${message.author.presence.status}`, true)
-      .addField("**Créer le:**", `${message.author.createdAt}`, true)
-      .setFooter(`KoKo's BOT`, bot.user.displayAvatarURL());
-  
-      message.channel.send({embed: uEmbed});
+        
+          let uEmbed = new Discord.MessageEmbed()
+          .setColor("#7289DA")
+        .setTitle("Server Info")
+        .setThumbnail(message.guild.iconURL())
+        .setAuthor(`${message.author.username} Info`, message.author.displayAvatarURL())
+        .addField("**Ismis:**", `${message.author.username}`, true)
+        .addField("**#:**", `${message.author.discriminator}`, true)
+        .addField("**ID:**", `${message.author.id}`, true)
+        .addField("**Status:**", `${message.author.presence.status}`, true)
+        .addField("**Créer le:**", `${message.author.createdAt}`, true)
+        .setFooter(`KoKo's Bot`, bot.user.displayAvatarURL());
+    
+        message.channel.send({embed: uEmbed});
     }  
     
     if (cmd === '.avatar') {
@@ -126,16 +175,24 @@ if(message.author.bot || message.channel.type==="dm")return;
   }
 }
 
-if(cmd===".help"){
-    let hEmbed = new Client.MessageEmbed()
-        .setColor("#7289DA")
-        .setThumbnail(bot.user.displayAvatarURL())
-        .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
-        .setDescription(`
+if (cmd === ".isreal" || cmd === ".Isreal") {
+  return message.reply("Isreal? you mean Palestine lmao")
+}
+
+
+if (cmd === ".help" ) {
+        let hEmbed = new Discord.MessageEmbed()
+            .setColor("#7289DA")
+            .setThumbnail(bot.user.displayAvatarURL())
+            .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
+            
+            .setDescription(`
 __**Commands List**__
 > \`say\` > **\`parler avec le bot\`**
 > \`userinfo\` > **\`info sur l'utilisateur\`**
-> \`cv\`, \`ui\`, \`avatar\``)
+> \`cv\`, \`avatar\`
+> \`stats\`
+`)
         
         message.channel.send({embed: hEmbed});
 }
@@ -167,6 +224,90 @@ if (!message.guild) return;
   }
 }
 
+bot.on('messageDelete', message => {
+    if(!message.partial) {
+        const channel = bot.channels.cache.get('713546625901002803');
+        if(channel) {
+            const embed = new MessageEmbed()
+                .setTitle('Deleted Message')
+                .addField('Author', `${message.author.tag} (${message.author.id})`, true)
+                .addField('Channel', `${message.channel.name} (${message.channel.id})`, true)
+                .setDescription(message.content)
+                .setTimestamp();
+            channel.send(embed);
+        }
+    }
+
+  
+});
+
+
+const usersMap = new Map();
+const LIMIT = 5;
+const TIME = 7000;
+const DIFF = 3000;
+
+bot.on('message', message => {
+  if(message.author.bot) return;
+  if(usersMap.has(message.author.id)) {
+    const userData = usersMap.get(message.author.id);
+    const { lastMessage, timer } = userData;
+    const difference = message.createdTimestamp - lastMessage.createdTimestamp;
+    let msgCount = userData.msgCount;
+    console.log(difference);
+    if(difference > DIFF) {
+      clearTimeout(timer);
+      console.log('Cleared timeout');
+      userData.msgCount = 1;
+      userData.lastMessage = message;
+      userData.timer = setTimeout(() => {
+        usersMap.delete(message.author.id);
+        console.log('Removed from RESET.');
+      }, TIME);
+      usersMap.set(message.author.id, userData);
+    }
+    else {
+      ++msgCount;
+      if(parseInt(msgCount) === LIMIT) {
+        const role = message.guild.roles.cache.get('712712501527052350');
+        message.member.roles.add(role);
+        message.channel.send('You have been muted.');
+        setTimeout(() => {
+          message.member.roles.remove(role);
+          message.channel.send('You have been unmuted');
+        }, TIME);
+      } else {
+        userData.msgCount = msgCount;
+        usersMap.set(message.author.id, userData);
+      }
+    }
+  }
+  else {
+    let fn = setTimeout(() => {
+      usersMap.delete(message.author.id);
+      console.log('Removed from map.');
+    }, TIME);
+    usersMap.set(message.author.id, {
+      msgCount: 1,
+      lastMessage: message,
+      timer: fn
+    });
+  }
+});
+
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+console.log(Date.now() + " Ping Received");
+response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+
 
 
 });  
@@ -176,4 +317,4 @@ if (!message.guild) return;
 
 
 
-bot.login("Njc4NjQ4NjM5NTEzNDI3OTc4.XsdLBA.9GRRVyHNIbgSe4NZoyr8czGbJDk"); 
+bot.login("Njc4NjQ4NjM5NTEzNDI3OTc4.Xsimww.tDlpuY0lUA-eHLuZtFiQ1kDmQnk"); 
